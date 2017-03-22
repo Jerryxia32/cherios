07f31a2821e540f76d4f99f3d8bf5e7f39e57c9c
hx242 <hx242@cam.ac.uk>
2017-03-22 17:11:36 +0000

    Avoid accidentally using $t2. Fill in ccallfast delay slots.

--- boot/src/elf_loader.c
+++ boot/src/elf_loader.c
@@ -107,7 +107,7 @@ int elf_check_supported(Elf_Env *env, Elf64_Ehdr *hdr) {
 		ERROR("Bad e_version");
 		return 0;
 	}
-	if(hdr->e_flags != 0x30C10007 && hdr->e_flags != 0x30C10001) {
+	if(hdr->e_flags != 0x30C10007 && hdr->e_flags != 0x30C10001 && hdr->e_flags != 0x30C20007 && hdr->e_flags != 0x30C20001) {
 		ERRORM("Bad e_flags: %X", hdr->e_flags);
 		return 0;
 	}

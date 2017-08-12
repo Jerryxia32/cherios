fbfc1628be99f54b3b6160fad6eaef7c54fd80ef
hx242 <hx242@cam.ac.uk>
2017-08-12 17:08:40 +0100

    Prettify the CHERI_PRINT_CAP outputs.

--- include/cheric.h
+++ include/cheric.h
@@ -227,14 +227,14 @@ cheri_zerocap(void)
 } while (0)
 
 #define CHERI_PRINT_PTR(ptr)						\
-	printf("%s: " #ptr " b:%016jx l:%016zx o:%jx\n", __func__,	\
+	printf("%s: " #ptr " b:0x%08zx l:0x%08zx o:0x%x\n", __func__,	\
 	   cheri_getbase((const void*__capability)(ptr)),		\
 	   cheri_getlen((const void*__capability)(ptr)),		\
 	   cheri_getoffset((const void*__capability)(ptr)))
 
 #define CHERI_PRINT_CAP(cap)						\
-	printf("%-20s: %-16s t:%lx s:%lx p:%08jx "			\
-	       "b:%016jx l:%016zx o:%jx\n",				\
+	printf("%-20s: %-16s t:%x s:%x p:%08x "			\
+	       "b:0x%08zx l:0x%08zx o:0x%x\n",				\
 	   __func__,							\
 	   #cap,							\
 	   cheri_gettag(cap),						\
@@ -245,7 +245,7 @@ cheri_zerocap(void)
 	   cheri_getoffset(cap))
 
 #define CHERI_PRINT_CAP_LITE(cap)					\
-	printf("t:%x s:%x b:0x%16jx l:0x%16zx o:0x%jx",			\
+	printf("t:%x s:%x b:0x%08zx l:0x%08zx o:0x%x",			\
 	   cheri_gettag(cap),						\
 	   cheri_getsealed(cap),					\
 	   cheri_getbase(cap),						\

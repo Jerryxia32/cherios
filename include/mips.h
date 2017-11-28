b3c426d2d441bfe40554eae18f756776ef1d7d8b
Hongyan Xia <Jerryxia32@gmail.com>
2017-11-28 14:43:31 +0000

    Use hash defines instead of numbers to access CP0 registers.

--- include/mips.h
+++ include/mips.h
@@ -32,6 +32,8 @@
 #ifndef _CHERIOS_MIPS_H_
 #define	_CHERIOS_MIPS_H_
 
+#include"asm.h"
+
 /*
  * Provide more convenient names for useful qualifiers from gcc/clang.
  */
@@ -41,7 +43,6 @@
 /*
  * 64-bit MIPS types.
  */
-#define REG_SIZE 8
 typedef unsigned long long  register_t;		/* 64-bit MIPS register */
 typedef unsigned int        paddr_t;		/* Physical address */
 typedef unsigned int        vaddr_t;		/* Virtual address */
@@ -105,55 +106,14 @@ typedef uint64_t	u_int64_t;
 /*
  * Useful addresses on MIPS.
  */
-#define	MIPS_BEV0_TLB_VECTOR	0x80000080
 #define	MIPS_BEV0_TLB_VECTOR_PTR	((void *)MIPS_BEV0_EXCEPTION_VECTOR)
-
-#define	MIPS_BEV0_EXCEPTION_VECTOR	0x80000180
 #define	MIPS_BEV0_EXCEPTION_VECTOR_PTR	((void *)MIPS_BEV0_EXCEPTION_VECTOR)
-
-#define	MIPS_BEV0_CCALL_VECTOR		0x80000280
 #define	MIPS_BEV0_CCALL_VECTOR_PTR	((void *)MIPS_BEV0_EXCEPTION_VECTOR)
 
 /*
  * Hard-coded MIPS interrupt numbers.
  */
 #define	MIPS_CP0_INTERRUPT_TIMER	7	/* Compare register. */
-
-/*
- * MIPS CP0 register numbers.
- */
-#define	MIPS_CP0_REG_INDEX		$0
-#define	MIPS_CP0_REG_RANDOM		$1
-#define	MIPS_CP0_REG_ENTRYLO0		$2
-#define	MIPS_CP0_REG_ENTRYLO1		$3
-#define	MIPS_CP0_REG_CONTEXT		$4
-#define	MIPS_CP0_REG_PAGEMASK		$5
-#define	MIPS_CP0_REG_WIRED		$6
-#define	MIPS_CP0_REG_RESERVED7		$7
-#define	MIPS_CP0_REG_BADVADDR		$8
-#define	MIPS_CP0_REG_COUNT		$9
-#define	MIPS_CP0_REG_ENTRYHI		$10
-#define	MIPS_CP0_REG_COMPARE		$11
-#define	MIPS_CP0_REG_STATUS		$12
-#define	MIPS_CP0_REG_CAUSE		$13
-#define	MIPS_CP0_REG_EPC		$14
-#define	MIPS_CP0_REG_PRID		$15
-#define	MIPS_CP0_REG_CONFIG		$16
-#define	MIPS_CP0_REG_LLADDR		$17
-#define	MIPS_CP0_REG_WATCHLO		$18
-#define	MIPS_CP0_REG_WATCHHI		$19
-#define	MIPS_CP0_REG_XCONTEXT		$20
-#define	MIPS_CP0_REG_RESERVED21		$21
-#define	MIPS_CP0_REG_RESERVED22		$22
-#define	MIPS_CP0_REG_RESERVED23		$23
-#define	MIPS_CP0_REG_RESERVED24		$24
-#define	MIPS_CP0_REG_RESERVED25		$25
-#define	MIPS_CP0_REG_ECC		$26
-#define	MIPS_CP0_REG_CACHEERR		$27
-#define	MIPS_CP0_REG_TAGLO		$28
-#define	MIPS_CP0_REG_TAGHI		$29
-#define	MIPS_CP0_REG_ERROREPC		$30
-#define	MIPS_CP0_REG_RESERVED31		$31
 
 /*
  * MIPS CP0 status register fields.

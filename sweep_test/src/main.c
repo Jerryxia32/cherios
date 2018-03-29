5c3c083657e2f6d3a2a6f10ed4c38c90f5bda4c5
Hongyan Xia <Jerryxia32@gmail.com>
2018-03-29 19:39:26 +0100

    Restructure sweep_test.
    Now it can process arbitrarily large core dump files.

--- sweep_test/src/main.c
+++ sweep_test/src/main.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
-#include<object.h>
-#include<namespace.h>
 #include<stdlib.h>
 #include<mips.h>
 #include<string.h>
 #include<cheric.h>
-#include<assert.h>
-#include<sha_info.h>
 #include<statcounters.h>
-#include<mibench_iter.h>
 
-uint32_t
+#define POOL_SIZE (64U << 20)
+#define CAPS_PER_LINE (CACHELINE_SIZE/CAP_SIZE)
+
+// This should be in .bss and zeroed.
+static statcounters_bank_t counter_sum_bank1;
+static statcounters_bank_t counter_sum_bank2;
+static statcounters_bank_t counter_start;
+static statcounters_bank_t counter_end;
+static statcounters_bank_t counter_diff;
+
+// simple random number generator
+static uint32_t
 gen_rand() {
   static uint64_t initial = 1727000011183999444ULL;
   initial = initial*6364136223846793005ULL + 1;
@@ -19,34 +25,64 @@ gen_rand() {
 
 void sweep_line(void*__capability, void*__capability);
 
+extern char __tags_bin_start, __tags_bin_end;
+
 int
 main() {
+  // the length of the total coredump memory size
+  size_t theLen = (&__tags_bin_end - &__tags_bin_start) << 6;
+  printf("The length of the coredump is %zd\n", theLen);
   void*__capability thePCC = cheri_getpcc();
-  void*__capability*__capability thePool = calloc_c(32U << 20, 1);
+  void*__capability*__capability thePool = calloc_c(POOL_SIZE, 1);
   printf("The length of the pool is %zd\n", cheri_getlen(thePool));
-  void*__capability testCap = calloc_c(32U << 10, 1);
-  for(int i=0; i<(1 << 19); i++) {
-    *(thePool+gen_rand()%(2 << 20)) = testCap;
-    //*(thePool+i) = testCap;
-  }
+  void*__capability testCap = calloc_c(16, 1);
 
-  stats_init();
-  void*__capability (*__capability sweeper)[16] = (void*__capability (*__capability)[16])thePool;
-  for(uint32_t i=0; i<(32U<<13); i++) {
-    if(cheri_getmultitag(sweeper)) {
-      sweep_line((void*__capability)sweeper, thePCC);
+  // the offset at which the next scan begins
+  size_t currentOffset = 0;
+
+  // We have limited memory in CheriRTOS, so only do the sweeping for a small
+  // fraction each time.
+  while(theLen > currentOffset) {
+    size_t lenToScan = (theLen-currentOffset)>POOL_SIZE? POOL_SIZE :
+        (theLen-currentOffset);
+
+    // Populate the POOL with capabilities.
+    for(size_t i=0; i<(lenToScan >> CAP_SIZE_BITS); i++) {
+      if((*(&__tags_bin_start+(i>>3)+(currentOffset>>6)) & (1<<(i & 0x7))) != 0)
+        *(thePool+i) = testCap;
+      else
+        *(thePool+i) = NULLCAP;
     }
-    sweeper++;
-  }
-  stats_display();
 
-  stats_init();
-  void*__capability*__capability sweeper2 = thePool;
-  for(uint32_t i=0; i<(32U<<13); i++) {
-    sweep_line((void*__capability)sweeper2, thePCC);
-    sweeper2 += 16;
+    void*__capability (*__capability sweeper)[CAPS_PER_LINE] =
+        (void*__capability (*__capability)[CAPS_PER_LINE])thePool;
+    sample_statcounters(&counter_start);
+    for(uint32_t i=0; i<(lenToScan>>CACHELINE_SIZE_BITS); i++) {
+      if(cheri_getmultitag(sweeper)) {
+        sweep_line((void*__capability)sweeper, thePCC);
+      }
+      sweeper++;
+    }
+    sample_statcounters(&counter_end);
+    diff_statcounters(&counter_end, &counter_start, &counter_diff);
+    // Add the diff to the final results.
+    add_statcounters(&counter_diff, &counter_sum_bank1, &counter_sum_bank1);
+
+    void*__capability*__capability sweeper2 = thePool;
+    sample_statcounters(&counter_start);
+    for(uint32_t i=0; i<(lenToScan>>CACHELINE_SIZE_BITS); i++) {
+      sweep_line((void*__capability)sweeper2, thePCC);
+      sweeper2 += CAPS_PER_LINE;
+    }
+    sample_statcounters(&counter_end);
+    diff_statcounters(&counter_end, &counter_start, &counter_diff);
+    add_statcounters(&counter_diff, &counter_sum_bank2, &counter_sum_bank2);
+
+    currentOffset += POOL_SIZE;
   }
-  stats_display();
+
+  dump_statcounters(&counter_sum_bank1, NULL, NULL);
+  dump_statcounters(&counter_sum_bank2, NULL, NULL);
 
   return 0;
 }
